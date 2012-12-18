41ca23523b2a8cb120aeb698f379ffc34d02f7cf
jeffhuys <jeffhuys@gmail.com>
2012-12-18 15:33:48 +0100

    Wiimote integratie begin

--- TurboPRT/src/turboprt/Podcar.java
+++ TurboPRT/src/turboprt/Podcar.java
@@ -9,227 +9,210 @@ import java.util.ArrayList;
  * @author marcel
  */
 public class Podcar {
-	private int id;
-	private String name;
-	private Location location;
-	private ArrayList<Passenger> passengers = new ArrayList<Passenger>();
-	private ArrayList<Destination> destinations = new ArrayList<Destination>();
-	private Status status;
-	private String macAddress;
-	
-	public BluetoothService btService;
-	private boolean connected = false;
-	
-	public enum Status {DRIVING, WAITING, CHARGING, BLOCKED, DISCONNECTED};
-	
-	private ArrayList<PodcarListener> listeners = new ArrayList<PodcarListener>();
 
-	Podcar()
-	{
-		this.id = -1;
-		this.location = new Location();
-	}
-	
-	public boolean connect()
-	{
-		try
-		{
-			System.out.println("Connecting to "+this.macAddress+"...");
-			btService = new BluetoothService();
-			if (btService.connectToDevice(this.macAddress, 1)) {
-				System.out.println("Connected to "+this.macAddress);
-				this.connected = true;
-				this.status = Status.WAITING;
-				
-				new StreamListener(this);
-				
-				broadcastChange();
-				return true;
-			} else {
-				System.out.println("Failed to connect to "+this.macAddress);
-				return false;
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("Couldn't connect to "+this.macAddress);
-			return false;
-		}
-	}
-	
-	public void disconnect()
-	{
-		this.btService.disconnect();
-		this.connected = false;
-		
-		setStatus(Status.DISCONNECTED);
-		
-		System.out.println(this.getName()+" got disconnected.");
-	}
-	
-	public void sendCommand(String command)
-	{
-		if(this.connected == true)
-		{
-			System.out.println("Sending command to "+this.macAddress+": "+command);
-			btService.sendCommand(command);
-		}
-		else
-		{
-			System.out.println("Device is not connected!");
-		}
-	}
-	
-	public void processCommand(String command)
-	{
-		System.out.println(""+command);
-		if(command.equals("ATDRV") && this.getStatus() != Podcar.Status.BLOCKED)
-		{
-			if(this.getStatus() == Podcar.Status.WAITING)
-			{
-				this.setStatus(Podcar.Status.DRIVING);
-			}
-			else
-			{
-				this.setStatus(Podcar.Status.WAITING);
-			}
-		}
-		else if(command.equals("E666"))
-		{
-			this.setStatus(Podcar.Status.BLOCKED);
-		}
-		else if(command.equals("!E666"))
-		{
-			this.setStatus(Podcar.Status.WAITING);
-		}
-	}
-	
-	public void turn(int direction) {
-		// turn
-	}
-	
-	public void blinkIR() {
-		
-	}
-	
-	
-	public void addListener(PodcarListener listener)
-	{
-		this.listeners.add(listener);
-	}
-	public void removeListener(PodcarListener listener)
-	{
-		this.listeners.remove(listener);
-	}
-	
-	private void broadcastChange()
-	{
-		for(PodcarListener listener : this.listeners)
-		{
-			listener.update(this);
-		}
-	}
-	
-	
-	public int getId() {
-		return id;
-	}
+    private int id;
+    private String name;
+    private Location location;
+    private ArrayList<Passenger> passengers = new ArrayList<Passenger>();
+    private ArrayList<Destination> destinations = new ArrayList<Destination>();
+    private Status status;
+    private String macAddress;
+    private boolean IR = false;
+    public BluetoothService btService;
+    private boolean connected = false;
 
-	public void setId(int id) {
-		this.id = id;
-		broadcastChange();
-	}
+    void setIR(boolean b) {
+        this.IR = b;
+    }
+    
+    boolean getIR() {
+        return this.IR;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public enum Status {
 
-	public void setName(String name) {
-		this.name = name;
-		broadcastChange();
-	}
-	
-	public Location getLocation() {
-		return location;
-	}
+        DRIVING, WAITING, CHARGING, BLOCKED, DISCONNECTED
+    };
+    private ArrayList<PodcarListener> listeners = new ArrayList<PodcarListener>();
 
-	public void setLocation(Location location) {
-		this.location = location;
-		broadcastChange();
-	}
+    Podcar() {
+        this.id = -1;
+        this.location = new Location();
+    }
 
-	public ArrayList<Passenger> getPassengers() {
-		return passengers;
-	}
+    public boolean connect() {
+        try {
+            System.out.println("Connecting to " + this.macAddress + "...");
+            btService = new BluetoothService();
+            if (btService.connectToDevice(this.macAddress, 1)) {
+                System.out.println("Connected to " + this.macAddress);
+                this.connected = true;
+                this.status = Status.WAITING;
 
-	public void setPassengers(ArrayList<Passenger> passengers) {
-		this.passengers = passengers;
-		broadcastChange();
-	}
+                new StreamListener(this);
 
-	public ArrayList<Destination> getDestinations() {
-		return destinations;
-	}
+                broadcastChange();
+                return true;
+            } else {
+                System.out.println("Failed to connect to " + this.macAddress);
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Couldn't connect to " + this.macAddress);
+            return false;
+        }
+    }
 
-	public void setDestinations(ArrayList<Destination> destinations) {
-		this.destinations = destinations;
-		broadcastChange();
-	}
-	
-	public void addDestination(Destination dest) {
-		this.destinations.add(dest);
-		broadcastChange();
-	}
-	
-	public void removeDestination(Destination dest) {
-		this.destinations.remove(dest);
-		broadcastChange();
-	}
+    public void disconnect() {
+        this.btService.disconnect();
+        this.connected = false;
 
-	public boolean isDriving() {
-		return (status == Status.DRIVING);
-	}
-	
-	public boolean isAvailable() {
-		return (status != Status.DRIVING && status != Status.BLOCKED);
-	}
+        setStatus(Status.DISCONNECTED);
 
-	public Status getStatus()
-	{
-		return this.status;
-	}
-	
-	public String getStatusString()
-	{
-		if(status == Status.CHARGING)
-			return "Charging";
-		else if(status == Status.DRIVING)
-			return "Driving";
-		else if(status == Status.BLOCKED)
-			return "Blocked";
-		else if(status == Status.DISCONNECTED)
-			return "Disconnected";
-		else
-			return "Waiting";
-	}
-	
-	public void setStatus(Status status) {
-		this.status = status;
-		broadcastChange();
-	}
+        System.out.println(this.getName() + " got disconnected.");
+    }
 
-	public boolean isConnected() {
-		return connected;
-	}
+    public void sendCommand(String command) {
+        if (this.connected == true) {
+            System.out.println("Sending command to " + this.macAddress + ": " + command);
+            btService.sendCommand(command);
+        } else {
+            System.out.println("Device is not connected!");
+        }
+    }
 
-	public String getMacAddress() {
-		return macAddress;
-	}
+    public void processCommand(String command) {
+        System.out.println("" + command);
+        if (command.equals("ATDRV") && this.getStatus() != Podcar.Status.BLOCKED) {
+            if (this.getStatus() == Podcar.Status.WAITING) {
+                this.setStatus(Podcar.Status.DRIVING);
+            } else {
+                this.setStatus(Podcar.Status.WAITING);
+            }
+        } else if (command.equals("E666")) {
+            this.setStatus(Podcar.Status.BLOCKED);
+        } else if (command.equals("!E666")) {
+            this.setStatus(Podcar.Status.WAITING);
+        }
+    }
 
-	public void setMacAddress(String macAddress) {
-		this.macAddress = macAddress;
-		broadcastChange();
-	}
-	
-	
+    public void turn(int direction) {
+        // turn
+    }
+
+    public void blinkIR() {
+    }
+
+    public void addListener(PodcarListener listener) {
+        this.listeners.add(listener);
+    }
+
+    public void removeListener(PodcarListener listener) {
+        this.listeners.remove(listener);
+    }
+
+    private void broadcastChange() {
+        for (PodcarListener listener : this.listeners) {
+            listener.update(this);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+        broadcastChange();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        broadcastChange();
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+        broadcastChange();
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+        broadcastChange();
+    }
+
+    public ArrayList<Destination> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(ArrayList<Destination> destinations) {
+        this.destinations = destinations;
+        broadcastChange();
+    }
+
+    public void addDestination(Destination dest) {
+        this.destinations.add(dest);
+        broadcastChange();
+    }
+
+    public void removeDestination(Destination dest) {
+        this.destinations.remove(dest);
+        broadcastChange();
+    }
+
+    public boolean isDriving() {
+        return (status == Status.DRIVING);
+    }
+
+    public boolean isAvailable() {
+        return (status != Status.DRIVING && status != Status.BLOCKED);
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public String getStatusString() {
+        if (status == Status.CHARGING) {
+            return "Charging";
+        } else if (status == Status.DRIVING) {
+            return "Driving";
+        } else if (status == Status.BLOCKED) {
+            return "Blocked";
+        } else if (status == Status.DISCONNECTED) {
+            return "Disconnected";
+        } else {
+            return "Waiting";
+        }
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+        broadcastChange();
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+        broadcastChange();
+    }
 }
--- TurboPRT/src/turboprt/Tracker.java
+++ TurboPRT/src/turboprt/Tracker.java
@@ -2,6 +2,23 @@ package turboprt;
 
 import bluetooth.BluetoothService;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import wiiusej.WiiUseApiManager;
+import wiiusej.Wiimote;
+import wiiusej.wiiusejevents.physicalevents.ExpansionEvent;
+import wiiusej.wiiusejevents.physicalevents.IREvent;
+import wiiusej.wiiusejevents.physicalevents.MotionSensingEvent;
+import wiiusej.wiiusejevents.physicalevents.WiimoteButtonsEvent;
+import wiiusej.wiiusejevents.utils.WiimoteListener;
+import wiiusej.wiiusejevents.wiiuseapievents.ClassicControllerInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.ClassicControllerRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.DisconnectionEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.GuitarHeroInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.GuitarHeroRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.NunchukInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.NunchukRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.StatusEvent;
 
 /**
  *
@@ -9,110 +26,231 @@ import java.util.ArrayList;
  */
 public class Tracker extends Thread implements PodcarListener {
 
-	private static ArrayList<Podcar> podcars = new ArrayList<Podcar>();
-	private BluetoothService device = null;
+    public static ArrayList<Podcar> podcars = new ArrayList<Podcar>();
+    private BluetoothService device = null;
+    private Wiimote wiimote;
+    WiiTracker trackerThread = new WiiTracker();
 
-	Tracker() {
+    Tracker() {
 
-		Podcar podcar;
+        System.out.println("Connecting to wiimote");
+        Wiimote[] wiimotes = WiiUseApiManager.getWiimotes(1, true);
+        System.out.println("wiimotes found: " + wiimotes.length);
+        wiimote = wiimotes[0];
+        wiimote.activateIRTRacking();
+        wiimote.addWiiMoteEventListeners(new WiiTracker());
 
-		// Charmender
-		podcar = new Podcar();
+        Podcar podcar;
+
+        trackerThread.start();
+/*
+        // Charmender
+        podcar = new Podcar();
 //		podcar.setId(1);
-		podcar.setName("Charmender");
-		podcar.setMacAddress("0007809B2AF9");
-		this.addPodcar(podcar);
+        podcar.setName("Charmender");
+        podcar.setMacAddress("0007809B2AF9");
+        this.addPodcar(podcar);
+        
+       
+         // N3liver
+         podcar = new Podcar();
+         //		podcar.setId(2);
+         podcar.setName("N3liver");
+         podcar.setMacAddress("00078096E0E1");
+         this.addPodcar(podcar);
 
-		// N3liver
-		podcar = new Podcar();
-//		podcar.setId(2);
-		podcar.setName("N3liver");
-		podcar.setMacAddress("00078096E0E1");
-		this.addPodcar(podcar);
+         // WT11-A
+         podcar = new Podcar();
+         //		podcar.setId(3);
+         podcar.setName("WT11-A");
+         podcar.setMacAddress("0007804C4657");
+         this.addPodcar(podcar);
 
-		// WT11-A
-		podcar = new Podcar();
-//		podcar.setId(3);
-		podcar.setName("WT11-A");
-		podcar.setMacAddress("0007804C4657");
-		this.addPodcar(podcar);
+         // Cyndaquil
+         podcar = new Podcar();
+         //		podcar.setId(4);
+         podcar.setName("Cyndaquil");
+         podcar.setMacAddress("0007804C4730");
+         this.addPodcar(podcar);
+         */
+        
+         // WT-12A
+         podcar = new Podcar();
+         //		podcar.setId(4);
+         podcar.setName("WT 12-A");
+         podcar.setMacAddress("00078096E0E1");
+         this.addPodcar(podcar);
+    }
 
-		// Cyndaquil
-		podcar = new Podcar();
-//		podcar.setId(4);
-		podcar.setName("Cyndaquil");
-		podcar.setMacAddress("0007804C4730");
-		this.addPodcar(podcar);
-	}
+    public void locatePodcar(int id) {
+    }
 
-	public void locatePodcar(int id) {
-	}
+    private void addPodcar(Podcar p) {
+        this.podcars.add(p);
+    }
 
-	private void addPodcar(Podcar p) {
-		this.podcars.add(p);
-	}
+    private void removePodcar(Podcar p) {
+        this.podcars.remove(p);
+    }
 
-	private void removePodcar(Podcar p) {
-		this.podcars.remove(p);
-	}
+    private ArrayList<Podcar> getPodcars() {
+        return podcars;
+    }
 
-	private ArrayList<Podcar> getPodcars() {
-		return podcars;
-	}
+    @Override
+    public void run() {
 
-	@Override
-	public void run() {
+        ArrayList<Thread> threadArray = new ArrayList<Thread>();
 
-		ArrayList<Thread> threadArray = new ArrayList<Thread>();
-		
-		for(final Podcar device : this.podcars)
-		{
-			// Register for podcar updates
-			device.addListener(this);
-			
-			Thread thread = new Thread(){
+        for (final Podcar device : this.podcars) {
+            // Register for podcar updates
+            device.addListener(this);
 
-				@Override
-				public void run() {
-					super.run();
-					
-					// Connect
-					device.connect();
-					
-					if(device.isConnected())
-					{
-						// Add this new bot to the UI
-						TurboPRT.gui.addRow(device);
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    super.run();
 
-						// Stuur 2 BS commands zodat ie de rest wel pakt
-						device.sendCommand("nop");
-						device.sendCommand("nop");
-					}
-				}
-			};
-			
-			thread.start();
-			threadArray.add(thread);
-		}
-	}
-	
-	public static Podcar getPodcarById(int id) throws Exception
-	{
-		for(Podcar device : podcars)
-		{
-			if(device.getId() == id)
-				return device;
-		}
-		
-		throw new Exception("Unknown podcar");
-	}
+                    // Connect
+                    device.connect();
 
-	/*
-	 * Update UI on podcar data change
-	 */
-	@Override
-	public void update(Podcar device) {
-		if(device.isConnected())
-			TurboPRT.gui.setDeviceLine(device);
-	}
+                    if (device.isConnected()) {
+                        // Add this new bot to the UI
+                        TurboPRT.gui.addRow(device);
+
+                        // Stuur 2 BS commands zodat ie de rest wel pakt
+                        device.sendCommand("nop");
+                        device.sendCommand("nop");
+                    }
+                }
+            };
+
+            thread.start();
+            threadArray.add(thread);
+        }
+    }
+
+    public static Podcar getPodcarById(int id) throws Exception {
+        for (Podcar device : podcars) {
+            if (device.getId() == id) {
+                return device;
+            }
+        }
+
+        throw new Exception("Unknown podcar");
+    }
+
+    /*
+     * Update UI on podcar data change
+     */
+    @Override
+    public void update(Podcar device) {
+        if (device.isConnected()) {
+            TurboPRT.gui.setDeviceLine(device);
+        }
+    }
+}
+
+class WiiTracker extends Thread implements WiimoteListener {
+
+    int lastRequestedPodcar = 0;
+
+    @Override
+    public void onButtonsEvent(WiimoteButtonsEvent wbe) {
+        //System.out.println("Button pressed");
+    }
+
+    @Override
+    public void onIrEvent(IREvent ire) {
+        if (lastRequestedPodcar == -1) {
+            return;
+        }
+
+        System.out.println("IR: " + ire.getX() + " | " + ire.getY());
+        Location loc = new Location();
+        loc.setLatitude(ire.getX());
+        loc.setLongitude(ire.getY());
+
+
+        if (Tracker.podcars.isEmpty()) {
+            return;
+        }
+
+        try {
+            Tracker.getPodcarById(Tracker.podcars.get(lastRequestedPodcar).getId()).setLocation(loc);
+
+            // Turn off LED
+            if(Tracker.podcars.get(lastRequestedPodcar).getIR()) {
+                Thread.sleep(150);
+                Tracker.getPodcarById(Tracker.podcars.get(lastRequestedPodcar).getId()).sendCommand("#L00#L10");
+                Tracker.podcars.get(lastRequestedPodcar).setIR(false);
+            }
+            
+            Thread.sleep(1000);
+        } catch (Exception ex) {
+            Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            for (Podcar p : Tracker.podcars) {
+                System.out.println("Getting podcar " + p.getName());
+
+                lastRequestedPodcar = p.getId();
+
+                // Turn on LED
+                p.sendCommand("#L01#L11");
+                p.setIR(true);
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onMotionSensingEvent(MotionSensingEvent mse) {
+    }
+
+    @Override
+    public void onExpansionEvent(ExpansionEvent ee) {
+    }
+
+    @Override
+    public void onStatusEvent(StatusEvent se) {
+    }
+
+    @Override
+    public void onDisconnectionEvent(DisconnectionEvent de) {
+    }
+
+    @Override
+    public void onNunchukInsertedEvent(NunchukInsertedEvent nie) {
+    }
+
+    @Override
+    public void onNunchukRemovedEvent(NunchukRemovedEvent nre) {
+    }
+
+    @Override
+    public void onGuitarHeroInsertedEvent(GuitarHeroInsertedEvent ghie) {
+    }
+
+    @Override
+    public void onGuitarHeroRemovedEvent(GuitarHeroRemovedEvent ghre) {
+    }
+
+    @Override
+    public void onClassicControllerInsertedEvent(ClassicControllerInsertedEvent ccie) {
+    }
+
+    @Override
+    public void onClassicControllerRemovedEvent(ClassicControllerRemovedEvent ccre) {
+    }
 }

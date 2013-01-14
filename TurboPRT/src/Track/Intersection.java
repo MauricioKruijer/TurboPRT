256dfaf7d04559fdc6f8b109bf0f937a64971a4b
Mark Blaas <markblaas@me.com>
2013-01-14 20:41:49 +0100

    Intersection and TrackInit

---
+++ TurboPRT/src/Track/Intersection.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013 Mark Blaas. All rights reserved.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY WARRANTIES ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+ */
+package Track;
+
+import java.util.ArrayList;
+import turboprt.Location;
+/**
+ * 
+ *
+ * @author Mark Blaas
+ */
+public class Intersection {
+    public static final int numberOfIntersections = 14;
+    
+    private int[][] cost = new int[numberOfIntersections][numberOfIntersections];
+    
+    int ID;
+    Location location = new Location();
+    ArrayList<Intersection> connected;
+    
+    public Intersection(){
+        
+    }
+    
+    public Intersection(int ID, Location location){
+        this.ID = ID;
+        this.location = location;
+    }
+    
+    public void addConnection(Intersection connection, int cost){
+        this.connected.add(connection);
+        this.cost[connection.ID][this.ID] = cost;
+        this.cost[this.ID][connection.ID] = cost;
+        
+    }
+    
+}
---
+++ TurboPRT/src/Track/TrackInitializer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Mark Blaas. All rights reserved.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY WARRANTIES ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+ */
+package Track;
+
+import java.util.ArrayList;
+import turboprt.Location;
+
+/**
+ *
+ * @author Mark Blaas
+ */
+public class TrackInitializer {
+
+    int[][] locations = new int[Intersection.numberOfIntersections][Intersection.numberOfIntersections];
+    int[][][] connections = {
+        {{1, 5}, {4, 10}, {5, 10}},//Intersect0 connection {{from 0 to 1 cost 5},{from 0 to 4 cost 10}},
+        {{0, 1}, {2, 1}, {6, 5}} //Intersect1 connections  {{from  1 to 0 cost 1}, {from 1 to 2 cost 1}}
+    };
+
+    public void init() {
+
+        ArrayList<Intersection> intersections = new ArrayList<>();
+
+        //Loop to create all intersections
+        for (int i = 0; i < Intersection.numberOfIntersections; i++) {
+            intersections.add(i, new Intersection(i, new Location(locations[i][0], locations[i][1])));
+        }
+
+        
+        //Loop for creating connections between intersections...
+        for (int i = 0; i < Intersection.numberOfIntersections; i++) { //For every intersection,
+            for (int j = 0; j < connections.length; j++) { //For evry connection in connections[][][]
+                //Add the connection and cost to the intersection object. 
+                intersections.get(i).addConnection(intersections.get(connections[i][j][0]), connections[i][j][1]);
+            }
+
+        }
+    }
+}

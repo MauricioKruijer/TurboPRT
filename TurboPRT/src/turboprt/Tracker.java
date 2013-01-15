1e62f096de0087f14d26ed321589cd3b5186a944
Marcel Rummens <marcelrummens@gmail.com>
2013-01-15 12:55:09 +0100

    Fixed BT names + new BT module

--- TurboPRT/src/turboprt/Tracker.java
+++ TurboPRT/src/turboprt/Tracker.java
@@ -26,132 +26,113 @@ import wiiusej.wiiusejevents.wiiuseapievents.StatusEvent;
  */
 public class Tracker extends Thread implements PodcarListener {
 
-    public static ArrayList<Podcar> podcars = new ArrayList<Podcar>();
-    private BluetoothService device = null;
-    private Wiimote wiimote;
-    WiiTracker trackerThread = new WiiTracker();
+	public static ArrayList<Podcar> podcars = new ArrayList<Podcar>();
+	private BluetoothService device = null;
+	private Wiimote wiimote;
+	WiiTracker trackerThread = new WiiTracker();
 
-    Tracker() {
+	Tracker() {
 
 		System.err.close();
-		
-        System.out.println("Connecting to wiimote");
-        Wiimote[] wiimotes = WiiUseApiManager.getWiimotes(1, true);
-        System.out.println("wiimotes found: " + wiimotes.length);
-        wiimote = wiimotes[0];
-        wiimote.activateIRTRacking();
-        wiimote.addWiiMoteEventListeners(new WiiTracker());
+
+		System.out.println("Connecting to wiimote");
+		Wiimote[] wiimotes = WiiUseApiManager.getWiimotes(1, true);
+		System.out.println("wiimotes found: " + wiimotes.length);
+		wiimote = wiimotes[0];
+		wiimote.activateIRTRacking();
+		wiimote.addWiiMoteEventListeners(new WiiTracker());
 		wiimote.addWiiMoteEventListeners(new Track());
 
-        Podcar podcar;
+		Podcar podcar;
 
-        trackerThread.start();
-/**/
-        // Charmender
-        podcar = new Podcar();
-//		podcar.setId(1);
-        podcar.setName("Charmender");
-        podcar.setMacAddress("0007809B2AF9");
-        this.addPodcar(podcar);
-    /*    
-       
-         // N3liver
-         podcar = new Podcar();
-         //		podcar.setId(2);
-         podcar.setName("N3liver");
-         podcar.setMacAddress("00078096E0E1");
-         this.addPodcar(podcar);
+		trackerThread.start();
 
-         // WT11-A
-         podcar = new Podcar();
-         //		podcar.setId(3);
-         podcar.setName("WT11-A");
-         podcar.setMacAddress("0007804C4657");
-         this.addPodcar(podcar);
+		// Totodile
+		podcar = new Podcar();
+		podcar.setName("Totodile");
+		podcar.setMacAddress("0007804C463D");
+		this.addPodcar(podcar);
 
-         // Cyndaquil
-         podcar = new Podcar();
-         //		podcar.setId(4);
-         podcar.setName("Cyndaquil");
-         podcar.setMacAddress("0007804C4730");
-         this.addPodcar(podcar);
-         */
-        
-         // WT-12A
-         //podcar = new Podcar();
-         //		podcar.setId(4);
-         //podcar.setName("WT 12-A");
-         //podcar.setMacAddress("00078096E0E1");
-         //this.addPodcar(podcar);
-    }
+		// Chikorita
+		podcar = new Podcar();
+		podcar.setName("Chikorita");
+		podcar.setMacAddress("0007804C4657");
+		this.addPodcar(podcar);
 
-    public void locatePodcar(int id) {
-    }
+		// Cyndaquil
+		podcar = new Podcar();
+		podcar.setName("Cyndaquil");
+		podcar.setMacAddress("0007804C4730");
+		this.addPodcar(podcar);
+	}
 
-    private void addPodcar(Podcar p) {
-        this.podcars.add(p);
-    }
+	public void locatePodcar(int id) {
+	}
 
-    private void removePodcar(Podcar p) {
-        this.podcars.remove(p);
-    }
+	private void addPodcar(Podcar p) {
+		this.podcars.add(p);
+	}
 
-    private ArrayList<Podcar> getPodcars() {
-        return podcars;
-    }
+	private void removePodcar(Podcar p) {
+		this.podcars.remove(p);
+	}
 
-    @Override
-    public void run() {
+	private ArrayList<Podcar> getPodcars() {
+		return podcars;
+	}
 
-        ArrayList<Thread> threadArray = new ArrayList<Thread>();
+	@Override
+	public void run() {
 
-        for (final Podcar device : this.podcars) {
-            // Register for podcar updates
-            device.addListener(this);
+		ArrayList<Thread> threadArray = new ArrayList<Thread>();
 
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    super.run();
+		for (final Podcar device : this.podcars) {
+			// Register for podcar updates
+			device.addListener(this);
 
-                    // Connect
-                    device.connect();
+			Thread thread = new Thread() {
+				@Override
+				public void run() {
+					super.run();
 
-                    if (device.isConnected()) {
-                        // Add this new bot to the UI
-                        TurboPRT.gui.addRow(device);
+					// Connect
+					device.connect();
 
-                        // Stuur 2 BS commands zodat ie de rest wel pakt
-                        device.sendCommand("nop");
-                        device.sendCommand("nop");
-                    }
-                }
-            };
+					if (device.isConnected()) {
+						// Add this new bot to the UI
+						TurboPRT.gui.addRow(device);
 
-            thread.start();
-            threadArray.add(thread);
-        }
-    }
+						// Stuur 2 BS commands zodat ie de rest wel pakt
+						device.sendCommand("nop");
+						device.sendCommand("nop");
+					}
+				}
+			};
 
-    public static Podcar getPodcarById(int id) throws Exception {
-        for (Podcar device : podcars) {
-            if (device.getId() == id) {
-                return device;
-            }
-        }
+			thread.start();
+			threadArray.add(thread);
+		}
+	}
 
-        throw new Exception("Unknown podcar");
-    }
+	public static Podcar getPodcarById(int id) throws Exception {
+		for (Podcar device : podcars) {
+			if (device.getId() == id) {
+				return device;
+			}
+		}
 
-    /*
-     * Update UI on podcar data change
-     */
-    @Override
-    public void update(Podcar device) {
-        if (device.isConnected()) {
-            TurboPRT.gui.setDeviceLine(device);
-        }
-    }
+		throw new Exception("Unknown podcar");
+	}
+
+	/*
+	 * Update UI on podcar data change
+	 */
+	@Override
+	public void update(Podcar device) {
+		if (device.isConnected()) {
+			TurboPRT.gui.setDeviceLine(device);
+		}
+	}
 }
 
 /**
@@ -160,118 +141,120 @@ public class Tracker extends Thread implements PodcarListener {
  */
 class WiiTracker extends Thread implements WiimoteListener {
 
-    int lastRequestedPodcar = 0;
+	int lastRequestedPodcar = 0;
 
-    @Override
-    public void onButtonsEvent(WiimoteButtonsEvent wbe) {
-        //System.out.println("Button pressed");
-    }
+	@Override
+	public void onButtonsEvent(WiimoteButtonsEvent wbe) {
+		//System.out.println("Button pressed");
+	}
 
-    @Override
-    public void onIrEvent(IREvent ire) {
-        if (lastRequestedPodcar == -1) {
-            return;
-        }
+	@Override
+	public void onIrEvent(IREvent ire) {
+		if (lastRequestedPodcar == -1) {
+			return;
+		}
 
-        System.out.println("IR(" + lastRequestedPodcar + ": " + ire.getX() + " | " + ire.getY());
-        Location loc = new Location();
-        loc.setLatitude(ire.getX());
-        loc.setLongitude(ire.getY());
+		System.out.println("IR(" + lastRequestedPodcar + ": " + ire.getX() + " | " + ire.getY());
+		Location loc = new Location();
+		loc.setLatitude(ire.getX());
+		loc.setLongitude(ire.getY());
 
 
-        if (Tracker.podcars.isEmpty()) {
-            return;
-        }
+		if (Tracker.podcars.isEmpty()) {
+			return;
+		}
 
-        try {
-            Tracker.getPodcarById(Tracker.podcars.get(lastRequestedPodcar).getId()).setLocation(loc);
+		try {
+			Tracker.getPodcarById(Tracker.podcars.get(lastRequestedPodcar).getId()).setLocation(loc);
 
-            // Turn off LED
-            if(Tracker.podcars.get(lastRequestedPodcar).getIR()) {
-                Thread.sleep(500);
-                Tracker.getPodcarById(Tracker.podcars.get(lastRequestedPodcar).getId()).sendCommand("#L00#L10");
-                Tracker.podcars.get(lastRequestedPodcar).setIR(false);
-            }
-            
-            Thread.sleep(1000);
-        } catch (Exception ex) {
-            //Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+			// Turn off LED
+			if (Tracker.podcars.get(lastRequestedPodcar).getIR()) {
+				Thread.sleep(500);
+				Tracker.getPodcarById(Tracker.podcars.get(lastRequestedPodcar).getId()).sendCommand("#L00#L10");
+				Tracker.podcars.get(lastRequestedPodcar).setIR(false);
+			}
 
-    @Override
-    public void run() {
-        while (true) {
-            for (Podcar p : Tracker.podcars) {
-				if(!p.isConnected()) continue;
-				
-                System.out.println("Getting podcar " + p.getName());
-                lastRequestedPodcar = p.getId();
+			Thread.sleep(1000);
+		} catch (Exception ex) {
+			//Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-                // Turn on LED
-                p.sendCommand("#L01#L11");
-                p.setIR(true);
-                
-                while(p.getIR()) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
+	@Override
+	public void run() {
+		while (true) {
+			for (Podcar p : Tracker.podcars) {
+				if (!p.isConnected()) {
+					continue;
+				}
 
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+				System.out.println("Getting podcar " + p.getName());
+				lastRequestedPodcar = p.getId();
 
-    }
+				// Turn on LED
+				p.sendCommand("#L01#L11");
+				p.setIR(true);
 
-    @Override
-    public void onMotionSensingEvent(MotionSensingEvent mse) {
-    }
+				while (p.getIR()) {
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException ex) {
+						Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
+					}
+				}
 
-    @Override
-    public void onExpansionEvent(ExpansionEvent ee) {
-    }
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException ex) {
+					Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Logger.getLogger(WiiTracker.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
 
-    @Override
-    public void onStatusEvent(StatusEvent se) {
-    }
+	}
 
-    @Override
-    public void onDisconnectionEvent(DisconnectionEvent de) {
-    }
+	@Override
+	public void onMotionSensingEvent(MotionSensingEvent mse) {
+	}
 
-    @Override
-    public void onNunchukInsertedEvent(NunchukInsertedEvent nie) {
-    }
+	@Override
+	public void onExpansionEvent(ExpansionEvent ee) {
+	}
 
-    @Override
-    public void onNunchukRemovedEvent(NunchukRemovedEvent nre) {
-    }
+	@Override
+	public void onStatusEvent(StatusEvent se) {
+	}
 
-    @Override
-    public void onGuitarHeroInsertedEvent(GuitarHeroInsertedEvent ghie) {
-    }
+	@Override
+	public void onDisconnectionEvent(DisconnectionEvent de) {
+	}
 
-    @Override
-    public void onGuitarHeroRemovedEvent(GuitarHeroRemovedEvent ghre) {
-    }
+	@Override
+	public void onNunchukInsertedEvent(NunchukInsertedEvent nie) {
+	}
 
-    @Override
-    public void onClassicControllerInsertedEvent(ClassicControllerInsertedEvent ccie) {
-    }
+	@Override
+	public void onNunchukRemovedEvent(NunchukRemovedEvent nre) {
+	}
 
-    @Override
-    public void onClassicControllerRemovedEvent(ClassicControllerRemovedEvent ccre) {
-    }
+	@Override
+	public void onGuitarHeroInsertedEvent(GuitarHeroInsertedEvent ghie) {
+	}
+
+	@Override
+	public void onGuitarHeroRemovedEvent(GuitarHeroRemovedEvent ghre) {
+	}
+
+	@Override
+	public void onClassicControllerInsertedEvent(ClassicControllerInsertedEvent ccie) {
+	}
+
+	@Override
+	public void onClassicControllerRemovedEvent(ClassicControllerRemovedEvent ccre) {
+	}
 }
